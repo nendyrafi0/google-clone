1ba0dd24277977f8e5cc9d6fadc54eb4d1f35530
Nendy Rafi <nendyrafi@gmail.com>
2024-08-15 18:55:53 +0700

    add - react-player - react-loader-spinner - use-debounce
    changes
    - added darkmode to app.jsx
    - added BrowserRouter to main.jsx
    - temporarily added Routes and Route to app.jsx
    - added Link to Navbar,jsx
    
    new features
    - Light Mode/Dark Mode
    - Mode Switch Button
    - New Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 
 import { Navbar } from './components/Navbar'
-import { Routes } from './components/Routes'
 import { Footer } from './components/Footer'
+
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -12,8 +13,10 @@ const App = () => {
       <div className='bg-gray-100
       dark:bg-gray-900 dark:text-gray-200
       min-h-screen'>
-        <Navbar />
-        <Routes />
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+        <Routes>
+          <Route path='/' element={<Routes />}/>
+        </Routes>
         <Footer />
       </div>
     </div>
